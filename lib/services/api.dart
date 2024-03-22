632ccd72fe63a1869d168a79f8e1fb299897d0ec
mai <maishalan00000@gmail.com>
2024-03-22 12:57:09 +0200

    addding post method to api file

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -10,6 +11,26 @@ class Api {
     } else {
       throw Exception(
           "There is an error with StatusCode ${response.statusCode} at Api().get(...)");
+    }
+  }
+
+  Future<List<dynamic>> post(
+      {required String url,
+      required Map<String, dynamic> body,
+      @required token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+
+    if (response.statusCode == 200) {
+      List<dynamic> responseList = jsonDecode(response.body);
+      return responseList;
+    } else {
+      throw Exception(
+          "There is an error with StatusCode ${response.statusCode} at Api().post(...) while the body is ${response.body}");
     }
   }
 }
